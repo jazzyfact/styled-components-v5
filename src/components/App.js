7d9dd78deb29aa61ab9651768cbb933ff71ee365
jazzyfact <jazzyfact2000@gmail.com>
2022-12-31 22:33:42 +0900

    feat: GlobalStyle 전역스타일, 구글 폰트 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,24 @@
+import { Button } from "./common";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+ body{
+  background: white;
+  min-height: 100vh;
+  margin: 0;
+  color: black;
+  font-family: Georgia, 'Times New Roman', Times, serif;
+ }
+`;
+
 function App() {
-  return <h1>App</h1>;
+  return (
+    <>
+      <GlobalStyle />
+      <h1>App</h1>
+      <Button>test</Button>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/common/Button.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  color: white;
+  background: #f8049c;
+  font-weight: bolad;
+  padding: 8px;
+  border-radius: 4px;
+  font-size: 1em;
+  border: none;
+  width: 100%;
+  display: block;
+  white-space: none;
+
+  &:disabled {
+    background: #eee;
+    color: #666;
+  }
+`;
+
+export { Button };
